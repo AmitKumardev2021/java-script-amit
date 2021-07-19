647f88d7ffdbadd530654e2c543207fce42a6cec
Amit <crazyone.amit@gmail.com>
2021-07-19 12:28:29 +0530

    file update 3

--- day-13/script.js
+++ day-13/script.js
@@ -1,3 +1,14 @@
-let x = Math.random();
+
+// random math operation 
+
+// let x = Math.floor(Math.random()*20)+1;
+
+// console.log(x);
+
+
+
+// date & time 
+let x = new Date(2019,07,02,10,);
 
 console.log(x);
+document.write(x);
