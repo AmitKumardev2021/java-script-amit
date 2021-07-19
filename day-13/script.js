b6be5f6fd8bd3b250c6aca5a0108ab2dd79bc782
Amit <crazyone.amit@gmail.com>
2021-07-19 14:22:38 +0530

    file update 4

--- day-13/script.js
+++ day-13/script.js
@@ -8,7 +8,35 @@
 
 
 // date & time 
-let x = new Date(2019,07,02,10,);
+// let x = new Date(2019,07,02,10,);
 
-console.log(x);
-document.write(x);
+// console.log(x);
+// document.write(x);
+
+// new key words
+
+// let person = new Object();
+
+// person.name = "Amit kumar";
+
+// person.age = 29;
+
+// console.log(person.name);
+// console.log(person.age);
+// document.write(person.name)
+
+
+
+let person ={
+    name:"Amit",
+    age: 29,
+    get getName() {
+        return this.name.toUpperCase();
+
+    }
+     
+
+
+};
+
+console.log(person.getName);
