0fd59be2254d8f657206c57b775c8abe6e133222
Amit <crazyone.amit@gmail.com>
2021-07-20 20:08:19 +0530

    file update 7

---
+++ day-14/script.js
@@ -0,0 +1,73 @@
+// console.log("hi Amit");
+
+// dom Event
+
+// inline click opertation
+
+// function btnClick(){
+//     alert("you click the button");
+// }
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click",btnClick);
+
+//annonyus function
+
+// let btn = document.getElementById("btn");
+// btn.addEventListener("click", function () {
+//   console.log("you are amit");
+// });
+
+// btn.addEventListener('mouseover',function(){
+//     console.log("mouse is activated");
+// });
+
+// btn.addEventListener('mouseout',function(){
+//     console.log("mouse is out bound");
+// });
+
+// window.addEventListener('DOMContentLoaded',function(){
+//     console.log('Dom tree connected');
+// })
+
+// window.addEventListener('load',function(){
+//     console.log('Succesfully loaded');
+// })
+
+
+// window.addEventListener('wheel',function(amitEvent){
+
+//     if(amitEvent.deltaY < 0){
+//         console.log("scrolling UP");
+//     }else if(amitEvent.deltaY>0){
+//         console.log("scrolling down");
+//     }
+    
+// // })
+
+// let d = document.getElementById("mydiv");
+// let b = document.getElementById("btn");
+
+
+// // btn funcction start from here 
+// function btnclick(amit){
+// console.log("button clicked...");
+// amit.stopPropagation();
+// }
+
+// // div function start from here 
+// function divclick(amit){
+// console.log("div clicked...");
+// amit.stopPropagation();
+
+// };
+// // body function start from here 
+// function bodyclick(){
+// console.log("body clicked...");
+// };
+
+// b.addEventListener('click',btnclick);
+// d.addEventListener('click',divclick);
+// document.body.addEventListener('click',bodyclick);
+
+
